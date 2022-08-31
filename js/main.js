a2adb317de0c4f4f1e3a64ab7a99c7b3ff36db56
yumsn <gin728emma218@gmail.com>
2022-08-31 22:57:28 +0900

    スライダーの修正

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
 'use strict'
 
 {
-    $('.work__slider').slick({
+    $('.pickup-slider').slick({
         arrows: false,
         autoplay: true, // 自動でスクロール
         autoplaySpeed: 2000, // 自動再生のスライド切り替えまでの時間を設定
         speed: 5000, // スライドが流れる速度を設定
         slidesToShow: 1,
         centerMode: true,
-        centerPadding: '60px',
-        responsive: [
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 1
-            }
-          }
-        ]
+        centerPadding: '64px',
+        // responsive: [
+        //   {
+        //     breakpoint: 600,
+        //     settings: {
+        //       slidesToShow: 1
+        //     }
+        //   }
+        // ]
       });
 }
