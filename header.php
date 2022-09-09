7d2321beb39a41b3781583eab98e6f1c76a4435f
yumsn <gin728emma218@gmail.com>
2022-09-09 11:00:07 +0900

    アナリティクスの追加

--- header.php
+++ header.php
@@ -8,6 +8,16 @@
     <title><?php echo bloginfo('name');?></title>
     <meta name="description" content="<?php bloginfo('description'); ?>">
 
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-VPGVKVDYHG"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-VPGVKVDYHG');
+    </script>
+
     <?php wp_head();?>
 </head>
 <body>
